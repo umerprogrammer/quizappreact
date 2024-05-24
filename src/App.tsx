eb7b4056aafa69f9be801a830d641ee197a1647c
mumerqsol <mumer.qsol@gmail.com>
2024-05-24 09:08:34 +0500

    Quiz App React

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import QuizScreen from './components/QuizScreen';
 
 function App() {
+  var questions = [
+    {
+      question: "HTML Stands For _________",
+      options: [
+        "Anchor Text Language",
+        "HTML",
+        "Case Cading Style Sheet",
+        "HyperText markup language",
+      ],
+      correctAns: "HyperText markup language",
+    },
+    {
+      question: "CSS Stands For _________",
+      options: [
+        "Casecading Style Sheet",
+        "Java",
+        "Ram",
+        "Hypertext markup language",
+      ],
+      correctAns: "Casecading Style Sheet",
+    },
+    {
+      question: "JS Stands For _________",
+      options: ["Java Style", "Java Script", "Script", "Script Src"],
+      correctAns: "Java Script",
+    },
+    {
+      question: "DOM Stands For _________",
+      options: ["Document Object Model", "html", "Css", "Java"],
+      correctAns: "Document Object Model",
+    },
+    {
+      question: "RAM Stands For _________",
+      options: ["Read Only Memory", "Dom", "Random Access Memory", "For Pc"],
+      correctAns: "Random Access Memory",
+    },
+    {
+      question: "ROM Stands For _________",
+      options: [
+        "Hyper Text Markup Language",
+        "html",
+        "HTml",
+        "Read Only Memory",
+      ],
+      correctAns: "Read Only Memory",
+    },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <QuizScreen quizArray={questions}/>
       </header>
     </div>
   );
---
+++ src/components/QuizScreen.tsx
@@ -0,0 +1,61 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { useState } from "react";
+
+export default function QuizScreen(props:{
+    //inputQuest:string;
+    // option1:string;
+    // option2:string;
+    // option3:string;
+    // option4:string;
+    quizArray?:any;
+}) {
+    //,option1,option2,option3,option4
+    const {quizArray} = props;
+    const [currentIndex,setCurrentIndex] = useState(0);
+  return (
+    <div className="conatiner-fluid g-0 rounded-4 shadow border border-light w-75 p-5">
+      <div className="container w-100">
+        <div className="row mb-3 d-flex justify-content-between">
+            <div className="col-md-12">  
+        <label htmlFor="Quest" className="text-start ">Question :</label>
+        <label htmlFor="">{currentIndex+1}/{quizArray.length}</label>
+        </div>
+        </div>
+        <div className="row mb-3">
+            <div className="col-md-12">
+                <div className="form-group text-start">        
+                   
+                    <input value={quizArray[currentIndex].question} className="form-control form-control-sm py-2" type="text" name="question" id="Quest" disabled />
+                </div>
+            </div>
+        </div>
+        <div className="row mb-3">
+        {
+        
+            quizArray[currentIndex].options.map((x:any,i:any)=>(
+                <div className="col-md-6">
+                 <button type="button" name="option1" className="btn border-light rounded-pill text-white px-5 w-100">{x}</button>
+                </div>
+                
+            ))}
+       
+           
+        </div>
+      
+        <div className="d-flex gap-3 justify-content-center">
+        <button type="button" className="btn btn-secondary px-5" onClick={()=>{
+            setCurrentIndex(
+                currentIndex -1 < 0 ? 0 : currentIndex-1
+                
+            );
+        }}>Back</button>
+        <button type="button" className="btn btn-primary px-5" onClick={()=>{
+            setCurrentIndex(
+              (currentIndex + 1) > (quizArray.length-1) ? (quizArray.length-1) : currentIndex+1)
+        }}>Next</button>
+        </div>
+      </div>
+    </div>
+  );
+}
